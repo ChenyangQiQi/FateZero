519bea67f586b9ca742d47a41e582a688b05eb4e
chenyangqi <chenyangqi67@gmail.com>
2023-04-06 19:40:02 +0800

    update download bash

---
+++ ckpt/download.sh
@@ -0,0 +1,8 @@
+# download from huggingface face, takes 20G space
+git lfs install
+
+git clone https://huggingface.co/CompVis/stable-diffusion-v1-4
+git clone https://huggingface.co/runwayml/stable-diffusion-v1-5
+git clone https://huggingface.co/chenyangqi/jeep_tuned_200
+git clone https://huggingface.co/chenyangqi/man_skate_250
+git clone https://huggingface.co/chenyangqi/swan_150
---
+++ data/download.sh
@@ -0,0 +1,17 @@
+wget https://github.com/ChenyangQiQi/FateZero/releases/download/v1.0.0/attribute.zip
+mkdir attribute
+unzip attribute.zip -d attribute
+
+wget https://github.com/ChenyangQiQi/FateZero/releases/download/v1.0.0/negative_reg.zip
+mkdir negative_reg
+unzip negative_reg.zip -d negative_reg
+
+wget https://github.com/ChenyangQiQi/FateZero/releases/download/v1.0.0/style.zip
+mkdir style
+unzip style.zip -d style
+
+wget https://github.com/ChenyangQiQi/FateZero/releases/download/v1.0.0/shape.zip
+mkdir shape
+unzip shape.zip -d shape
+
+
